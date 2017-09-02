94e5cac013c5340899bffa3b5a0b3567d2c9a875
petitsurume <git@surume.tk>
2017-09-02 15:23:41 +0900

    first

---
+++ src/main/java/tk/surume/mcmod/recipejson/RecipeJson.java
@@ -0,0 +1,20 @@
+package tk.surume.mcmod.recipejson;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = RecipeJson.MODID, version = RecipeJson.VERSION)
+public class RecipeJson
+{
+    public static final String MODID = "recipejson";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
