d074890e7da150f3c0db775cab28673b8d994b20
petitsurume <git@surume.tk>
2017-09-02 18:23:21 +0900

    とりあえずJSON読み込んでoutをパースするところまでは出来た

--- src/main/java/tk/surume/mcmod/recipejson/RecipeJson.java
+++ src/main/java/tk/surume/mcmod/recipejson/RecipeJson.java
@@ -1,9 +1,25 @@
 package tk.surume.mcmod.recipejson;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 
 @Mod(modid = RecipeJson.MODID, version = RecipeJson.VERSION)
 public class RecipeJson
@@ -11,10 +27,61 @@ public class RecipeJson
     public static final String MODID = "recipejson";
     public static final String VERSION = "1.0";
     
+    static String convertInputStreamToString(InputStream is) throws IOException {
+        InputStreamReader reader = new InputStreamReader(is);
+        StringBuilder builder = new StringBuilder();
+        char[] buffer = new char[512];
+        int read;
+        while (0 <= (read = reader.read(buffer))) {
+            builder.append(buffer, 0, read);
+        }
+        return builder.toString();
+    }
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void init(FMLInitializationEvent event) throws IOException
     {
-		// some example code
+        // コンフィグロード
+        File configFile = new File("./config/recipe.json");
+        if (configFile.exists() == false) { // デフォルトコンフィグのコピー
+            configFile.createNewFile();
+            FileWriter writer = new FileWriter(configFile);
+            InputStream defaultConfigStream = this.getClass().getClassLoader().getResourceAsStream("recipe.default.json");
+            writer.write(convertInputStreamToString(defaultConfigStream));
+            writer.close();
+        }
+        FileReader reader = new FileReader(configFile);
+        JsonArray recipes = (JsonArray) new Gson().fromJson(reader, JsonArray.class);
+        for (int i = 0; i < recipes.size(); i++) {
+            JsonObject recipe = recipes.get(i).getAsJsonObject();
+            ItemStack outItem;
+            // 出力アイテムのチェック
+            JsonElement outItemElement = recipe.get("out");
+            if(outItemElement == null) {
+                throw new RuntimeException(String.valueOf(i+1)+"番目のレシピの”out”が指定されていません");
+            }
+            String outItemName = outItemElement.getAsString();
+            if(Item.itemRegistry.containsKey(outItemName) == false) {
+                throw new RuntimeException(String.valueOf(i+1)+"番目のレシピの”out”に"+
+                    "指定されているアイテム”"+outItemName+"”が見つかりませんでした");
+            }
+            outItem = new ItemStack(Item.class.cast(Item.itemRegistry.getObject(outItemName)));
+            GameRegistry.addRecipe(outItem,
+                " x ",
+                "xxx",
+                " x ",
+                'x', outItem);
+        }
+        reader.close();
+
+        System.out.println(Item.itemRegistry.containsKey("minecraft:dirrt"));
+
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        GameRegistry.addRecipe(new ItemStack(Items.diamond, 1),
+            " x ",
+            "xxx",
+            " x ",
+            'x', new ItemStack(Item.class.cast(Item.itemRegistry.getObject("minecraft:dirrrt")))
+        );
     }
 }
